3514282ce523690c59ffbb7aad55a0d2a90524ba
Ninja-Recai <santana.kwasniewska@gmail.com>
2018-09-05 20:45:17 +0200

    Perform cleanup
    Delete `action creators factory` and action creators
    in favor of using zeal-redux-utils for consistency and
    a better tested solution

--- client/src/utils/action-creators/action-creators-factory.js
+++
@@ -1,8 +0,0 @@
-import { zipObj } from 'ramda';
-
-export const actionCreatorsFactory = (actions) => {
-  const actionCreatorsFunctions = Object.values(actions)
-    .map(action => payload => ({ type: action, payload }));
-  const actionCreatorsKeys = Object.keys(actions);
-  return zipObj(actionCreatorsKeys, actionCreatorsFunctions);
-};
--- client/src/utils/actions/actions-factory.js
+++
@@ -1,3 +0,0 @@
-import { zipObj } from 'ramda';
-
-export const actionsFactory = (prefix, actions) => zipObj(actions, actions.map(action => `@@${prefix}/${action}`));

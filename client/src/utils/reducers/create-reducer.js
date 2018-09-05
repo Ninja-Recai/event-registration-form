0fe1bf57db0f8c1c222e5ff83b847634aefc4567
Ninja-Recai <santana.kwasniewska@gmail.com>
2018-09-05 20:37:50 +0200

    Use zeal-redux-utils `createReducer` function
    Getting rid of custom code in favor of using
    a tested and well-checked util (already used in
    the project).

--- client/src/utils/reducers/create-reducer.js
+++
@@ -1,7 +0,0 @@
-export const createReducer = (initialState, actionsMap) => (state = initialState, action = {}) => {
-  const reducerFn = actionsMap[action.type];
-  if (reducerFn) {
-    return reducerFn(state, action);
-  }
-  return state;
-};
